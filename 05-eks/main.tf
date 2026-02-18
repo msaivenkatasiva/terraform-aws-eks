700d71c465b459985262b434d889f13b18db1aed
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-18 12:17:26 +0530

    aws-eks

--- 05-eks/main.tf
+++ 05-eks/main.tf
@@ -41,20 +41,7 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    # blue = {
-    #   min_size      = 2
-    #   max_size      = 10
-    #   desired_size  = 2
-    #   capacity_type = "SPOT"
-    #   iam_role_additional_policies = {
-    #     AmazonEBSCSIDriverPolicy          = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-    #     AmazonElasticFileSystemFullAccess = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
-    #     ElasticLoadBalancingFullAccess = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
-    #   }
-    #   # EKS takes AWS Linux 2 as it's OS to the nodes
-    #   key_name = aws_key_pair.eks.key_name
-    # }
-    green = {
+    blue = {
       min_size      = 2
       max_size      = 10
       desired_size  = 2
@@ -67,6 +54,19 @@ module "eks" {
       # EKS takes AWS Linux 2 as it's OS to the nodes
       key_name = aws_key_pair.eks.key_name
     }
+    # green = {
+    #   min_size      = 2
+    #   max_size      = 10
+    #   desired_size  = 2
+    #   capacity_type = "SPOT"
+    #   iam_role_additional_policies = {
+    #     AmazonEBSCSIDriverPolicy          = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+    #     AmazonElasticFileSystemFullAccess = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
+    #     ElasticLoadBalancingFullAccess = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+    #   }
+    #   # EKS takes AWS Linux 2 as it's OS to the nodes
+    #   key_name = aws_key_pair.eks.key_name
+    # }
   }
 
   tags = var.common_tags
